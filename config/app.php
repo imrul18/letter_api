09af315d82e18c0b122377890e1ad77bad1442de
imrul18 <imrul.afnan18@gmail.com>
2023-05-25 03:12:41 +0600

    update

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        'Intervention\Image\ImageServiceProvider'
     ])->toArray(),
 
     /*
@@ -184,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => 'Intervention\Image\Facades\Image',
     ])->toArray(),
 
 ];
